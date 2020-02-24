adafc6cab6c8e03c7412e15e9f568deb89a90d27
romluc <romulofilhofilho@gmail.com>
2020-02-24 13:47:33 -0300

    Build navbarmenu dynamically

--- js/app.js
+++ js/app.js
@@ -20,8 +20,7 @@
 
 const navBarList = document.querySelector('#navbar__list');
 const navBarMenu = document.querySelector('.navbar__menu');
-const menuLinks = ['home', 'profile', 'contact us'];
-console.log(navBarList);
+const menuLinks = ['home', 'profile', 'product', 'contact us'];
 
 /**
  * End Global Variables
@@ -38,11 +37,36 @@ console.log(navBarList);
 // build the nav - based on the array of menu items (menuLinks)
 menuLinks.forEach(link => {
   const newLi = document.createElement('li');
-  newLi.innerHTML = `${link}`;
+  const newHr = document.createElement('hr');
+  newLi.textContent = `${link}`;
   newLi.classList.add('menu__link');
   navBarList.appendChild(newLi);
+  navBarList.appendChild(newHr);
 });
-nnavBarMenu.appendChild(navBarList);
+navBarMenu.appendChild(navBarList);
+
+// build search box with a input and a button inside
+const newSearchBox = document.createElement('div');
+newSearchBox.classList.add('search-box');
+
+const newSearchBoxInput = document.createElement('input');
+newSearchBoxInput.setAttribute('type', 'Text');
+newSearchBoxInput.classList.add('search-box__input');
+newSearchBoxInput.setAttribute('placeholder', 'Search...');
+
+const newSearchBoxButton = document.createElement('button');
+newSearchBoxButton.setAttribute('type', 'submit');
+newSearchBoxButton.classList.add('search-box__button');
+
+const newSearchBoxButtonI = document.createElement('i');
+newSearchBoxButtonI.classList.add('fas');
+newSearchBoxButtonI.classList.add('fa-search');
+
+newSearchBox.appendChild(newSearchBoxInput);
+newSearchBox.appendChild(newSearchBoxButton);
+newSearchBoxButton.appendChild(newSearchBoxButtonI);
+
+navBarMenu.appendChild(newSearchBox);
 
 // Add class 'active' to section when near top of viewport
 
