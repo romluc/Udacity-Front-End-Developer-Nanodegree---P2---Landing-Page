eae09ee7b1b5d95f81952eb034ddb88dacf61bb5
romluc <romulofilhofilho@gmail.com>
2020-03-01 16:11:31 -0300

    Adjust max-width for larger viewports

--- js/app.js
+++ js/app.js
@@ -9,34 +9,49 @@ const navBarMenu = document.querySelector('.navbar__menu');
 const menuLinks = ['home', 'profile', 'product', 'services', 'contact'];
 const sections = ['section1', 'section2', 'section3', 'section4'];
 
-let scrolling = true;
-
 /**
  * End Global Variables
  * Start Helper Functions
  *
  */
-var debounce = function(fn) {
-  // Setup a timer
-  var timeout;
 
-  // Return a function to run debounced
+let debounce = function(func, wait = 1000, immediate = true) {
+  let timeout;
   return function() {
-    // Setup the arguments
-    var context = this;
-    var args = arguments;
-
-    // If there's a timer, cancel it
-    if (timeout) {
-      window.cancelAnimationFrame(timeout);
-    }
-
-    // Setup the new requestAnimationFrame()
-    timeout = window.requestAnimationFrame(function() {
-      fn.apply(context, args);
-    });
+    var context = this,
+      args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
   };
 };
+
+// let debounce = function(fn) {
+//   // Setup a timer
+//   let timeout;
+
+//   // Return a function to run debounced
+//   return function() {
+//     // Setup the arguments
+//     var context = this;
+//     var args = arguments;
+
+//     // If there's a timer, cancel it
+//     if (timeout) {
+//       window.cancelAnimationFrame(timeout);
+//     }
+
+//     // Setup the new requestAnimationFrame()
+//     timeout = window.requestAnimationFrame(function() {
+//       fn.apply(context, args);
+//     });
+//   };
+// };
 
 function isVisible(elem) {
   let coords = elem.getBoundingClientRect();
@@ -51,6 +66,31 @@ function isVisible(elem) {
   return topVisible || bottomVisible;
 }
 
+// Check if the user stopped scrolling so that the page header can be hidden
+let scrollStop = function(callback) {
+  // Make sure a valid callback was provided
+  if (!callback || typeof callback !== 'function') return;
+
+  // Setup scrolling variable
+  let isScrolling;
+
+  // Listen for scroll events
+  window.addEventListener(
+    'scroll',
+    function() {
+      // Clear our timeout throughout the scroll
+      window.clearTimeout(isScrolling);
+
+      // Set a timeout to run after scrolling ends
+      isScrolling = setTimeout(function() {
+        // Run the callback
+        callback();
+      }, 2000);
+    },
+    false
+  );
+};
+
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -62,6 +102,8 @@ const buildNavList = () => {
   // button for the burger menu
   const menuButton = document.createElement('button');
   menuButton.classList.add('menu-btn');
+  menuButton.classList.add('fas');
+  menuButton.classList.add('fa-bars');
 
   menuLinks.forEach(link => {
     const newLi = document.createElement('li');
@@ -86,7 +128,7 @@ const buildNavList = () => {
     navBarList.appendChild(newLi);
     navBarList.appendChild(newHr);
   });
-  navBarMenu.appendChild(menuButton);
+  document.body.prepend(menuButton);
   navBarMenu.appendChild(navBarList);
 };
 
@@ -115,46 +157,6 @@ const buildSearchBox = () => {
   navBarMenu.appendChild(newSearchBox);
 };
 
-const buildSections = () => {
-  let idSection = 0;
-  const main = document.querySelector('main');
-  const newDivContainer = document.createElement('div');
-  newDivContainer.classList.add('container');
-
-  sections.forEach(element => {
-    idSection = sections.indexOf(element) + 1;
-    const newSection = document.createElement('section');
-    const newDiv = document.createElement('div');
-    const newH2 = document.createElement('h2');
-    const newFirstPara = document.createElement('p');
-    const newSecondPara = document.createElement('p');
-
-    newSection.setAttribute('id', `section${idSection}`);
-    newSection.setAttribute('data-nav', `Section ${idSection}`);
-
-    // Extracting sections names from menuLinks array and capitalizing first letter
-    const h2Title =
-      menuLinks[idSection].charAt(0).toUpperCase() +
-      menuLinks[idSection].slice(1);
-    newH2.textContent = `${h2Title}`;
-
-    newFirstPara.textContent = `Gathered by gravity prime number cosmic ocean how far away from which we spring white dwarf. The ash of stellar alchemy Orion's sword rich in heavy atoms the sky calls to us permanence of the stars courage of our questions? Not a sunrise but a galaxyrise rich in mystery courage of our questions network of wormholes inconspicuous motes of rock and gas dream of the mind's eye. Citizens of distant epochs encyclopaedia galactica vanquish the impossible sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem extraordinary claims require extraordinary evidence totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.`;
-    newSecondPara.textContent = `Muse about Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium invent the universe dream of the mind's eye star stuff harvesting star light Apollonius of Perga? Billions upon billions Sea of Tranquility shores of the cosmic ocean kindling the energy hidden in matter not a sunrise but a galaxyrise radio telescope. Ut enim ad minima veniam the carbon in our apple pies how far away great turbulent clouds courage of our questions inconspicuous motes of rock and gas and billions upon billions upon billions upon billions upon billions upon billions upon billions.`;
-
-    newDiv.classList.add('landing__container');
-    newDiv.appendChild(newH2);
-    newDiv.appendChild(newFirstPara);
-    newDiv.appendChild(newSecondPara);
-
-    newSection.appendChild(newDiv);
-
-    newDivContainer.appendChild(newSection);
-    main.appendChild(newDivContainer);
-  });
-};
-
-// Add class 'active' to section when near top of viewport
-
 const loadImage = () => {
   const newContainer = document.createElement('div');
   const newImg = document.createElement('div');
@@ -174,122 +176,49 @@ const loadImage = () => {
   header.appendChild(newContainer);
 };
 
-// Scroll to anchor ID using scrollTO event
+const buildSections = () => {
+  let idSection = 0;
+  const main = document.querySelector('main');
+  const newDivContainer = document.createElement('div');
+  newDivContainer.classList.add('container');
 
-// const hideNavWhenNotScrolling = () => {
-//   isScrolling = false;
-//   if (isScrolling) {
-//     document.querySelector('.page__header').classList.remove('hidden');
-//     window.clearTimeout(timer);
-//   }
+  sections.forEach(element => {
+    idSection = sections.indexOf(element) + 1;
+    const newSection = document.createElement('section');
+    const newDiv = document.createElement('div');
+    const newH2 = document.createElement('h2');
+    const newContent = document.createElement('div');
+    const newFirstPara = document.createElement('p');
+    const newSecondPara = document.createElement('p');
 
-//   timer = setTimeout(function() {
-//     if (!isScrolling) {
-//       document.querySelector('.page__header').classList.add('hidden');
-//     }
-//     console.log(isScrolling);
-//   }, 2000);
-// };
+    newSection.setAttribute('id', `section${idSection}`);
+    newSection.setAttribute('data-nav', `Section ${idSection}`);
 
-let scrollStop = function(callback) {
-  // Make sure a valid callback was provided
-  if (!callback || typeof callback !== 'function') return;
+    // Extracting sections names from menuLinks array and capitalizing first letter
+    const h2Title =
+      menuLinks[idSection].charAt(0).toUpperCase() +
+      menuLinks[idSection].slice(1);
+    newH2.textContent = `${h2Title}`;
 
-  // Setup scrolling variable
-  let isScrolling;
+    newFirstPara.textContent = `Gathered by gravity prime number cosmic ocean how far away from which we spring white dwarf. The ash of stellar alchemy Orion's sword rich in heavy atoms the sky calls to us permanence of the stars courage of our questions? Not a sunrise but a galaxyrise rich in mystery courage of our questions network of wormholes inconspicuous motes of rock and gas dream of the mind's eye. Citizens of distant epochs encyclopaedia galactica vanquish the impossible sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem extraordinary claims require extraordinary evidence totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.`;
+    newSecondPara.textContent = `Muse about Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium invent the universe dream of the mind's eye star stuff harvesting star light Apollonius of Perga? Billions upon billions Sea of Tranquility shores of the cosmic ocean kindling the energy hidden in matter not a sunrise but a galaxyrise radio telescope. Ut enim ad minima veniam the carbon in our apple pies how far away great turbulent clouds courage of our questions inconspicuous motes of rock and gas and billions upon billions upon billions upon billions upon billions upon billions upon billions.`;
 
-  // Listen for scroll events
-  window.addEventListener(
-    'scroll',
-    function() {
-      // Clear our timeout throughout the scroll
-      window.clearTimeout(isScrolling);
+    newContent.classList.add('content');
+    newContent.appendChild(newFirstPara);
+    newContent.appendChild(newSecondPara);
 
-      // Set a timeout to run after scrolling ends
-      isScrolling = setTimeout(function() {
-        // Run the callback
-        callback();
-        console.log('asdf');
-      }, 2000);
-    },
-    false
-  );
-};
+    newDiv.classList.add('landing__container');
+    newDiv.appendChild(newH2);
+    newDiv.appendChild(newContent);
 
-const hidePageHeaderIfNotScrolling = () => {
-  if (!document.querySelector('.page__header').classList.contains('hidden')) {
-    document.querySelector('.page__header').classList.add('hidden');
-  }
-  console.log('chamada');
-};
+    newSection.appendChild(newDiv);
 
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-document.addEventListener('DOMContentLoaded', function(event) {
-  let t0 = performance.now();
-
-  buildNavList();
-  loadImage();
-  buildSections();
-  buildSearchBox();
-  showBackToTopButton();
-
-  const sectionsArray = document.querySelectorAll('section');
-
-  const setSectionActive = () => {
-    sectionsArray.forEach(element => {
-      element.classList.remove('active');
-      if (isVisible(element)) {
-        element.classList.add('active');
-        console.log(element.firstChild.firstChild.textContent.toLowerCase());
-      }
-    });
-  };
-
-  let debouncedSetSectionActive = debounce(setSectionActive);
-  let i = 0;
-  window.addEventListener(
-    'scroll',
-    function() {
-      i++;
-      debouncedSetSectionActive();
-      console.log(`called ${i} times`);
-    },
-    false
-  );
-
-  let t1 = performance.now();
-  console.log(`Dynamic building took ${t1 - t0} milisseconds`);
-});
-
-// function showVisible() {
-//   for (let img of document.querySelectorAll('img')) {
-//     let realSrc = img.dataset.src;
-//     if (!realSrc) continue;
-
-//     if (isVisible(img)) {
-//       console.log('came here');
-//       img.src = realSrc;
-//       img.dataset.src = '';
-//     }
-//   }
-// }
-
-// Set sections as active
-const setTargetSectionActive = element => {
-  sectionsArray.forEach(section => {
-    section.classList.remove('active');
+    newDivContainer.appendChild(newSection);
+    main.appendChild(newDivContainer);
   });
-
-  console.log(`${element} is active`);
 };
 
-// Show a back to top button when user reaches window height
-const showBackToTopButton = () => {
+const buildBackToTopButton = () => {
   const buttonTop = document.createElement('button');
   const arrowI = document.createElement('i');
 
@@ -302,14 +231,69 @@ const showBackToTopButton = () => {
   buttonTop.appendChild(arrowI);
   document.body.appendChild(buttonTop);
 
-  buttonTop.addEventListener('click', function() {
-    window.scrollTo(pageYOffset, 0);
-    // after scrollTo, there will be a "scroll" event, so the arrow will hide automatically
+  return buttonTop;
+};
+
+// Show a back to top button when user reaches window height
+const showButtonBackToTop = buttonTop => {
+  if (pageYOffset > document.documentElement.clientHeight) {
+    buttonTop.classList.remove('hidden');
+  } else buttonTop.classList.add('hidden');
+
+  return buttonTop;
+};
+
+const hidePageHeaderIfNotScrolling = () => {
+  document.querySelector('.page__header').classList.add('hidden');
+};
+
+// Add class 'active' to section when near top of viewport
+const sectionsContent = document.querySelectorAll('.content');
+let i = 0;
+const setSectionActive = arr => {
+  i++;
+  console.log(`called ${i} times`);
+  arr.forEach(element => {
+    element.classList.remove('active');
+    if (isVisible(element)) {
+      element.classList.add('active');
+      console.log(element.firstChild.firstChild.textContent.toLowerCase());
+    }
   });
 
-  window.addEventListener('scroll', function() {
-    if (pageYOffset > document.documentElement.clientHeight) {
-      buttonTop.classList.remove('hidden');
-    } else buttonTop.classList.add('hidden');
-  });
+  return arr;
 };
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+buildNavList();
+buildSearchBox();
+loadImage();
+buildSections();
+
+const burgerButton = document.querySelector('.menu-btn');
+burgerButton.addEventListener('click', () => {
+  document.querySelector('ul.navbar__list').classList.add('show');
+});
+
+const buttonTop = buildBackToTopButton();
+const sectionsArr = document.querySelectorAll('.container > section');
+const header = document.querySelector('.page__header');
+
+buttonTop.addEventListener('click', function() {
+  window.scrollTo(pageYOffset, 0);
+});
+
+window.addEventListener('scroll', () => {
+  header.classList.remove('hidden');
+
+  showButtonBackToTop(buttonTop);
+  debounce(setSectionActive(sectionsArr));
+  // setSectionActive(sectionsArr);
+
+  // scrollStop(hidePageHeaderIfNotScrolling);
+});
